4725c64784a29c85bbea5607de569fd38ad34de3
unknown <mihowyllt@gmail.com>
2024-01-23 11:12:34 -0800

    New File

---
+++ form_1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    welcome <?php echo $_POST["firstname"], $_POST["lastname"] ?><br>
+
+    Your email address is: <?php echo $_POST["email"] ?><br>
+    Your address is: <?php echo $_POST["address"] ?><br>
+</body>
+</html>
